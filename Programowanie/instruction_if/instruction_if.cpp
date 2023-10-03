674b11b18a517a3abc895ad00e373eb446a4469a
Wers3t <wers3t@gmail.com>
2023-10-03 22:33:28 +0200

    dada

--- Programowanie/instruction_if/instruction_if.cpp
+++ Programowanie/instruction_if/instruction_if.cpp
@@ -130,21 +130,31 @@ void task6()
 	cin >> month;
 	cout << "Enter the year: " << endl;
 	cin >> year;
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11 && day > 0 && day < 32)
+	if (month >= 1 && month <= 12)
 	{
-		cout << "The date is correct." << endl;
-	}
-	else if (month == 4 || month == 6 || month == 8 || month == 10 || month == 12 && day > 0 && day < 31)
-	{
-		cout << "The date is correct." << endl;
-	}
-	else if (month == 2 && day >= 1 && day >= 28)
-	{
-		cout << "The date is correct." << endl;
-	}
-	else
-	{
-		cout << "The date isn't correct." << endl;
+		if (month == 2)
+		{
+			if (day >= 1 && day <= 28)
+			{
+				cout << "Date is correct." << endl;
+			}
+			else
+			{
+				cout << "Date isn't correct." << endl;
+			}
+		}
+		else if ((month == 4 || month == 6 || month == 9 || month == 11) && day >= 1 && day <= 30)
+		{
+			cout << "Date is correct." << endl;
+		}
+		else if (day >= 1 && day <= 31)
+		{
+			cout << "Date is correct." << endl;
+		}
+		else
+		{
+			cout << "Date isn't correct." << endl;
+		}
 	}
 }
 
