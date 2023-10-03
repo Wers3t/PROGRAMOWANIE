0f21bb7ef9a35ea1323673ab8983f55f4d30ef60
NIGGER <alanKYS@KYS.xyz>
2023-10-03 15:24:02 +0200

    13131

---
+++ Programowanie/instruction_if/instruction_if.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void task1()
+{
+	double firstNumber;
+	double secondNumber;
+	double quotient;
+	cout << "Enter the first number: " << endl;
+	cin >> firstNumber;
+	cout << "Enter the second number: " << endl;
+	cin >> secondNumber;
+	quotient = firstNumber * secondNumber;
+	cout << "Quotient score of two numbers is: " << quotient << endl;
+}
+
+int main()
+{
+	task1();
+}
+
+//Napisz funkcje ktora  podzieli dwie liczby 
