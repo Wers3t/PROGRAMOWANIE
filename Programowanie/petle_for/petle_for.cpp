1f08ecaa7446bd5dc0ee69c7057eb0599bdc56d2
NIGGER <alanKYS@KYS.xyz>
2023-11-07 16:23:25 +0100

    3ecdadada

--- Programowanie/petle_for/petle_for.cpp
+++ Programowanie/petle_for/petle_for.cpp
@@ -1,6 +1,7 @@
 ﻿
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -142,29 +143,80 @@ void task10()
 //*Program obliczający sumę kwadratów liczb od 1 do 10
 void task11()
 {
-	for (int i = 0; i <=10; i++)
+	int sum = 0;
+	for (int i = 0; i <= 10; i++)
 	{
+		sum = sum + i * i;
+	}
+	cout << sum;
+}
+//* Napisz program, który policzy sumę cyfr podanej przez użytkownika liczby.
+void task12()
+{
+	double numberFromUser = 0;
+	double sum = 0;
 
+	do
+	{
+		cout << "Enter number: " << endl;
+		cin >> numberFromUser;
+		sum = sum + numberFromUser;
+		cout << "Sum: " << sum << endl;
+	} while (true);
+}
+// Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.) dopóki suma tych kwadratów nie przekroczy 1000.
+void task13()
+{
+	int sum = 0;
+	for (int i = 1; sum < 1000; i++)
+	{
+		if (i >= 10)
+		{
+			i = 1;
+		}
+		sum = sum + i * i;
+		cout << sum << endl;
 	}
 }
-/*
-
-DO - WHILE
-* Napisz program, który policzy sumę cyfr podanej przez użytkownika liczby.
-* Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.) dopóki suma tych kwadratów nie przekroczy 1000.
+//* Program, kóry wyświetli poniższe cztery wzory :
+// *
+// **
+// ***
+// ****
+// *****
+void task14()
+{
+	int numberFromUser = 0;
+	cout << "Enter number: " << endl;
+	cin >> numberFromUser;
+	for (int i = 0; i <= numberFromUser; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+/* Napisz program, który poprosi użytkownika o wprowadzenie dowolnej liczby całkowitej.Następnie program powinien obliczyć i wyświetlić liczbę cyfr.
+*/
+void task15()
+{
+	int numberFromUser = 0;
+	cout << "Enter number from user: " << endl;
+	cin >> numberFromUser;
+	cout << to_string(numberFromUser).length();
+}
+/*DO - WHILE
 
 WHILE
 * Miasto T.ma obecnie 100 tys.mieszkańców, ale jego populacja rośnie co roku o 3 % rocznie.Miasto B.ma 300 tys.mieszkańców i ta liczba rośnie w tempie 2 % na rok.Wykonaj symulację prezentującą liczbę mieszkańców w obu miastach i zatrzymującą się, gdy liczba mieszkańców miasta T.przekroczy liczbę z miasta B.
-* Napisz program, który poprosi użytkownika o wprowadzenie dowolnej liczby całkowitej.Następnie program powinien obliczyć i wyświetlić liczbę cyfr.
+
 * Program sprawdzający czy podana liczba jest liczbą doskonałą(czyli taką, której suma dzielników(z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
 
 FOR
 * Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu(ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
-* Program, kóry wyświetli poniższe cztery zwory :
-****54321        121212        122333
-* **65432        212121        223334444
-* *76543        121212        333444455555
-* 87654        212121        444455555666666
+
 
 * Oblicz sumę szeregu 1 / 1 ^ 2 + 1 / 2 ^ 2 + 1 / 3 ^ 2 + ... + 1 / n ^ 2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π(suma szeregu jest równa π2 / 6).Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
 
@@ -182,6 +234,11 @@ int main()
 	//task7();
 	//task8();
 	//task9();
-	task10();
+	//task10();
+	//task11();
+	//task12();
+	//task13();
+	//task14();
+	//task15();
 }
 
