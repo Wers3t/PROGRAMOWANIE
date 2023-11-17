e03f65206d49c89c70ba1547ce73177409c8ba0e
NIGGER <alanKYS@KYS.xyz>
2023-11-17 13:17:40 +0100

    dada

---
+++ Programowanie/string_console_app/string_console_app.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+//napisz program ktory wczyta znak z klawiatury
+void task1()
+{
+	string characterFromUser;
+	cout << "Enter any character: " << endl;
+	cin >> characterFromUser;
+	cout << "Your character is: "<<'"'<< characterFromUser <<'"'<<"." << endl;
+}
+
+//napisz program ktory poprosi cie o twoje imie i cie przywita.
+void task2()
+{
+	string nameFromUser;
+	cout << "Enter your name: " << endl;
+	cin >> nameFromUser;
+	cout << "Hi " << nameFromUser <<"." << endl;
+}
+
+int main()
+{
+	//task1();
+	//task2();
+}
