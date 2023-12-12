08734fc1362dd264951911a30d4bd2531aa6e84c
NIGGER <alanKYS@KYS.xyz>
2023-12-12 16:44:49 +0100

    dada

--- Programowanie/array_console_application/array_console_application.cpp
+++ Programowanie/array_console_application/array_console_application.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+void task1()
+{
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+
+
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        cout << "Podaj liczbe\n";
+        cin >> numbers[i];
+    }
+   
+    for (int i = SIZE_OF_ARRAY - 1; i >= 0; i--)
+    {
+        cout << "Podana liczba " << numbers[i] << "\n";
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    task1();
 }
 
