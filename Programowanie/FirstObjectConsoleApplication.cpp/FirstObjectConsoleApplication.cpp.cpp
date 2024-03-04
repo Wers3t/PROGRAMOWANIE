483d423329877f75361e630bc4e9712935dcec23
NIGGER <alanKYS@KYS.xyz>
2024-03-04 11:39:54 +0100

    sdfassersdfda

--- Programowanie/FirstObjectConsoleApplication.cpp/FirstObjectConsoleApplication.cpp.cpp
+++ Programowanie/FirstObjectConsoleApplication.cpp/FirstObjectConsoleApplication.cpp.cpp
@@ -1,35 +1,118 @@
 ﻿#include <iostream>
-using namespace std;
 
-class point
+/*
+Zdefiniuj klase opisującą prostokąt. Możliwości klasy:
+* konstruktory
+* metoda która zwórci obwód prostokąta
+* metoda która pole prostokąta
+* metodę która pokaże informacje o prostokącie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+*/
+
+
+using namespace std;
+class Point
 {
+private:
+	int x;
+	int y;
+	int z;
+
 public:
-	double x;
-	double y;
-	double z;
+	Point()
+	{
+		x = 3;
+		y = 7;
+		z = 10;
+	}
+
+	Point(int xx)
+	{
+		x = xx;
+		y = 7;
+		z = 10;
+	}
+
+	Point(int xx, int yy, int zz)
+	{
+		x = xx;
+		y = yy;
+		z = zz;
+	}
+
+	void SetX(int value)
+	{
+		//x = abs(value);
+		if (value >= 0)
+			x = value;
+		else
+		{
+			//reakcja na błąd
+		}
+	}
+
+	int GetX()
+	{
+		//if (uprawnienia)
+
+		return x;
+	}
+
+	double DistanceFromCenter()
+	{
+		double distance;
+		distance = sqrt(x * x + y * y + z * z);
+		return distance;
+	}
 };
 
-double DistanceFromCenter(double x, double y, double z)
-{
-	double distance;
-	distance = sqrt(x * x + y * y + z * z);
-	return distance;
-}
 
-double DistanceFromCenter(point p)
+class Rectangle
 {
-	double distance;
-	distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
-	return distance;
-}
+private:
+	float lenght;
+	float height;
+public:
+	Rectangle(float l = 1, float h = 1)
+	{
+		lenght = l;
+		height = h;
+	}
+
+
+
+	float getArea() {
+		return lenght * height;
+	}
+
+	float getPerimeter() {
+		return lenght * 2 + height * 2;
+	}
+
+	void getInfo() {
+
+		cout << "Area: " << getArea() << endl;
+		cout << "Perimeter: " << getPerimeter() << endl;
+		cout << "Height: " << height << endl;
+		cout << "Lenght: " << lenght << endl;
+	}
+};
 
 int main()
 {
-	point firstPoint;
-	firstPoint.x = 3;
-	firstPoint.y = 5;
-	firstPoint.z = 8;
+	//Point firstPoint;
+	////firstPoint.x = 3;
+	//firstPoint.SetX(-3);
+	////firstPoint.y = 5;
+	////firstPoint.z = 8;
+	//cout << "Distance " << firstPoint.DistanceFromCenter() << "\n";
 
-	cout << "Distance from center: " << DistanceFromCenter(firstPoint.x, firstPoint.y, firstPoint.z) << endl;
-	cout << "Distance from center: " << DistanceFromCenter(firstPoint) << endl;
+	//Point secondPiont(15);
+	//cout << "Distance " << secondPiont.DistanceFromCenter() << "\n";
+
+	Rectangle firstRectangle;
+	cout << "Area: " << firstRectangle.getArea() << endl;
+	cout << "Perimeter: " << firstRectangle.getPerimeter() << endl;
+	firstRectangle.getInfo();
 }
