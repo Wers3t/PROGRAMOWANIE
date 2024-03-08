77bf788252fcbd994a52da106cdade472078a452
NIGGER <alanKYS@KYS.xyz>
2024-03-08 12:44:19 +0100

    wet4

--- Programowanie/FirstObjectConsoleApplication.cpp/FirstObjectConsoleApplication.cpp.cpp
+++ Programowanie/FirstObjectConsoleApplication.cpp/FirstObjectConsoleApplication.cpp.cpp
@@ -99,20 +99,112 @@ public:
 	}
 };
 
-int main()
+/*
+ZADANIE
+Zdefiniuj klase opisujące konto bankowe. Możliwości klasy:
+* konstruktory
+* metoda która dokona wpłaty na konto
+* metoda która wypłaty z konta
+* metoda która wykona przelew na inne konto
+* metodę która pokaże informacje o koncie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+*/
+
+
+class BankAccount
 {
-	//Point firstPoint;
-	////firstPoint.x = 3;
-	//firstPoint.SetX(-3);
-	////firstPoint.y = 5;
-	////firstPoint.z = 8;
-	//cout << "Distance " << firstPoint.DistanceFromCenter() << "\n";
+private:
+	string accountNumber;
+	string owner;
+	double balance;
+public:
+	//konstuktor
+	BankAccount(string number, string owner, double postal_balance = 0.00)
+	{
+		accountNumber = number;
+		this->owner = owner;
+		balance = postal_balance;
 
-	//Point secondPiont(15);
-	//cout << "Distance " << secondPiont.DistanceFromCenter() << "\n";
+	}
+	void Deposit(double sum)
+	{
+		balance += sum;
+	}
 
+	void Withdraw(double sum) {
+		if (balance >= sum) {
+			balance -= sum;
+			cout << "Withdrawed: " << sum << " PLN." << endl;
+		}
+		else {
+			cout << "There are insufficient funds in your account." << endl;
+		}
+	}
+
+	void
+		Transfer(double sum, string targetAccount)
+	{
+		if (balance >= sum)
+		{
+			balance -= sum;
+			cout << "Funds were transferred to account: " << targetAccount << " in a sum of: " << sum << " PLN." << endl;
+
+		}
+		else
+		{
+			cout << "Insufficient funds in the account" << endl;
+		}
+
+	}
+
+	void ShowInfo()
+	{
+		cout << "Account number: " << accountNumber << endl;
+		cout << "Account owner: " << owner << endl;
+		cout << "Balance: " << balance << endl;
+	}
+};
+
+void ShowBankInfo()
+{
+	BankAccount account("1234567890", "Jan Kowalski", 1000.0);
+
+	account.ShowInfo();
+
+	account.Deposit(500.0);
+	account.ShowInfo();
+
+	account.Deposit(200.0);
+	account.ShowInfo();
+
+	account.Transfer(300.0, "0987654321");
+	account.ShowInfo();
+}
+
+
+void rectExercises()
+{
 	Rectangle firstRectangle;
 	cout << "Area: " << firstRectangle.getArea() << endl;
 	cout << "Perimeter: " << firstRectangle.getPerimeter() << endl;
 	firstRectangle.getInfo();
+}
+
+
+int main()
+{
+	//point firstpoint;
+	//firstpoint.x = 3;
+	//firstpoint.setx(-3);
+	//firstpoint.y = 5;
+	//firstpoint.z = 8;
+	//cout << "distance " << firstpoint.distancefromcenter() << "\n";
+
+	//point secondpiont(15);
+	//cout << "distance " << secondpiont.distancefromcenter() << "\n";
+
+	//rectExercises();
+
+	ShowBankInfo();
 }
