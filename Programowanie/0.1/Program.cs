531c443c3ed8f786614a2befc7c7206f57888712
q <q>
2024-10-29 15:54:28 +0100

    dada

--- Programowanie/0.1/Program.cs
+++ Programowanie/0.1/Program.cs
@@ -9,7 +9,7 @@ person.Name = "Ewa";
 person.Age = 32;
 person.Surname = "Kowalska";
 
-people.Add(person); 
+people.Add(person);
 
 
 person = new Person();
@@ -24,7 +24,7 @@ person.Name = "Karol";
 person.Age = 52;
 person.Surname = "Krawczyk";
 
-people.Add(person); 
+people.Add(person);
 
 person = new Person();
 person.Name = "Paweł";
@@ -38,13 +38,43 @@ people.Add(person);
 
 Console.WriteLine("Glowa kolekcja: ");
 
-for (int i = 0; i < people.Count; i++)
-{
-    Console.WriteLine($"Imie: {people[i].Name} Nazwisko: {people[i].Surname} Wiek: {people[i].Age}  ");
-}
+//for (int i = 0; i < people.Count; i++)
+//{
+//    Console.WriteLine($"Imie: {people[i].Name} Nazwisko: {people[i].Surname} Wiek: {people[i].Age}  ");
+//}
 
-
-foreach(Person p in people)
+foreach (Person p in people)
 {
     Console.WriteLine($"Imie: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}  ");
 }
+
+var sortedPeopleByAge = people.OrderBy(p => p.Age);
+Console.WriteLine("Kolekcja po sortowana po wieku:");
+
+foreach (Person p in sortedPeopleByAge)
+{
+    Console.WriteLine($"Imie: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}  ");
+}
+
+sortedPeopleByAge = people.OrderByDescending(p => p.Age);
+Console.WriteLine("Kolekcja po sortowana po wieku malejaco:");
+
+foreach (Person p in sortedPeopleByAge)
+{
+    Console.WriteLine($"Imie: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}  ");
+}
+
+Console.WriteLine();
+int maxAge = people.Max(p => p.Age);
+Console.WriteLine($"Najstarsza osoba ma  {maxAge} lat.");
+Console.WriteLine();
+
+double averageAge = people.Average(p => p.Age);
+Console.WriteLine($"Sredni wiek to  {averageAge} lat.");
+Console.WriteLine();
+
+
+
+
+
+
