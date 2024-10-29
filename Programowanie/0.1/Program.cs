26b6747ebe872a8609cdd0086f471011add7f4a3
q <q>
2024-10-29 15:20:46 +0100

    i

---
+++ Programowanie/0.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using LinqConsoleApp;
+
+List<Person> people = new List<Person>();
+
+#region Uzupełnienie kolekcji
+
+Person person = new Person();
+person.Name = "Ewa";
+person.Age = 32;
+person.Surname = "Kowalska";
+
+people.Add(person); 
+
+
+person = new Person();
+person.Name = "Jan";
+person.Age = 12;
+person.Surname = "Nowak";
+
+people.Add(person);
+
+person = new Person();
+person.Name = "Karol";
+person.Age = 52;
+person.Surname = "Krawczyk";
+
+people.Add(person); 
+
+person = new Person();
+person.Name = "Paweł";
+person.Age = 35;
+person.Surname = "Nowakowski";
+
+people.Add(person);
+
+
+#endregion
+
+Console.WriteLine("Glowa kolekcja: ");
+
+for (int i = 0; i < people.Count; i++)
+{
+    Console.WriteLine($"Imie: {people[i].Name} Nazwisko: {people[i].Surname} Wiek: {people[i].Age}  ");
+}
+
+
+foreach(Person p in people)
+{
+    Console.WriteLine($"Imie: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}  ");
+}
