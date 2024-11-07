23c3a7e792a475fca14177bb5cd033f4f4724846
q <q>
2024-11-07 10:32:56 +0100

    da

--- Programowanie/0.1/Program.cs
+++ Programowanie/0.1/Program.cs
@@ -33,6 +33,15 @@ person.Surname = "Nowakowski";
 
 people.Add(person);
 
+person = new Person();
+person.Name = "Paweł";
+person.Age = 5;
+person.Surname = "Nowak";
+
+person = new Person();
+person.Name = "Karol"; 
+person.Age = 23;
+person.Surname = "Nowak";
 
 #endregion
 
@@ -73,8 +82,26 @@ double averageAge = people.Average(p => p.Age);
 Console.WriteLine($"Sredni wiek to  {averageAge} lat.");
 Console.WriteLine();
 
+Console.Clear();
 
+List<Person> sortedPeople = people.OrderBy(p => p.Surname).ThenByDescending(p => p.Age).ToList();
 
+Console.WriteLine( "Kolekcja posortowana: ");
+foreach(Person p in sortedPeople)
+{
+    Console.WriteLine($"Imie: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+}
+sortedPeople = people.OrderBy(p => new Random().Next()).ToList();
 
+if (people.All(people => people.Age >= 18))
+{
+    Console.WriteLine("Wszystkie osoby sa pelnoletnie");
+}
 
+Person firstPerson = people.FirstOrDefault(p => p.Name.Length == 3);
 
+if (firstPerson.Equals(default(Person)) == false)
+{
+    Console.WriteLine("osoba z imieniem wiekszym niz trzy litery");
+    Console.WriteLine($"{firstPerson.Name}");
+}
