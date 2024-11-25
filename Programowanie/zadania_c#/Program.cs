d929ba2b595d583b1e187d7a355c8567b08ded5e
q <q>
2024-11-25 16:02:54 +0100

    dada

--- Programowanie/zadania_c#/Program.cs
+++ Programowanie/zadania_c#/Program.cs
@@ -1,4 +1,6 @@
 ﻿using LinqTasksConsoleApp;
+using System;
+using System.ComponentModel.Design;
 
 var tasks = new List<TaskItem>
 {
@@ -46,8 +48,51 @@ Console.WriteLine(countedCompited);
 //zadanie 5
 Console.WriteLine("Zadanie 5");
 
-var tasksName = (tasks.name).ToList();
-// select name
-// tasks
+var tasksName = tasks.Select(t => t.Name).ToList();
 
+foreach ( var task in tasksName)
+{
+    Console.WriteLine(task);
+}
+//zadanie 6
+// Zadanie 6: Znalezienie nazw zakończonych zadań posortowanych według długości nazwy
+Console.WriteLine("Zadanie 6");
 
+var sortedByCount = tasks.Where(t => t.IsCompleted).OrderBy(t => t.Name.Length).Select(t => t.Name);
+foreach (var task in sortedByCount)
+{
+    Console.WriteLine(task);
+}
+
+//zadanie 7
+//Zadanie 7: Zadania pogrupowane według stanu zakończenia, a następnie posortowane w grupach według nazwy
+Console.WriteLine("Zadanie 7");
+
+var groupByCompleted = tasks.OrderBy(t => t.IsCompleted).ThenBy(t => t.Name);
+foreach (var task in groupByCompleted)
+{
+    Console.WriteLine(task);
+}
+
+//zadanie 8
+//Zadanie 8: Najkrótsza nazwa zadania niezakończonego
+Console.WriteLine("Zadanie 8");
+
+var shortestNameNotComplited = tasks.Where(t => t.IsCompleted == false).OrderBy(t => t.Name.Length).Select(t => t.Name).First();
+if(shortestNameNotComplited != null)
+    {
+        Console.WriteLine(shortestNameNotComplited);
+    }
+
+//zadanie 9
+//Zadanie 9: Ilość liter w nazwach wszystkich zakończonych zadań
+Console.WriteLine("Zadanie 9");
+
+var countedLettersInComplited = tasks.Where(t => t.IsCompleted).Select(t => t.Name.Length);
+foreach (var task in countedLettersInComplited)
+{
+Console.WriteLine(task);
+}
+
+//zadanie 10
+//Zadanie 10: Lista zadań z indeksami (zakończone zadania z numeracją)
